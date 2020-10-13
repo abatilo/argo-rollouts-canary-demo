10329f01d77b31f877f782349db70ec359a0072d
Aaron <AaronBatilo@gmail.com>
2020-10-13 05:19:33 -0600

    Implement preStop to delay termination of canary pods

--- cmd/pingserver/main.go
+++ cmd/pingserver/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
@@ -17,6 +22,18 @@ func main() {
 		Handler: h,
 	}
 
+	c := make(chan os.Signal, 1)
+	done := make(chan struct{})
+	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-c
+		log.Println("Shutting down")
+		time.Sleep(30 * time.Second)
+		s.Shutdown(context.Background())
+		close(done)
+	}()
+
 	log.Println("Listening...")
 	s.ListenAndServe()
+	<-done
 }
